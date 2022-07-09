c30cd75bbbbd34aaa2e87a096cc67022b638f074
Surviville <ultimatesurviville@gmail.com>
2022-07-09 14:43:10 +0200

    New minting website, rewritten walletOfOwner

--- docs/js/settings-test.js
+++ docs/js/settings-test.js
@@ -2,7 +2,7 @@
 // deployed on testnet from 0xE0d61627d14F1E6C1C5FABDD219dF0a003C9a0cC
 
 window.RewardTokenAddress  = '0x95c3adFE28E2BAB89B285AB058c450F61B938661';
-window.ReaperAddress       = '0x4C40680ab319411a0F98Fb5DBc7105E64da12B18';
+window.ReaperAddress       = '0xb428AD05f925B82d0d7172EA50BeD9d03764de06';
 window.DevAddress          = '0xE0d61627d14F1E6C1C5FABDD219dF0a003C9a0cC';
 window.rpcServer           = 'https://moeing.tech:9545';
 window.netId               = 10001;
