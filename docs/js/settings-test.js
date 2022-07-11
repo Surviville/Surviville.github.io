b9e6ea761d4091cfaf5269bdbfb6648045166a8f
Surviville <ultimatesurviville@gmail.com>
2022-07-11 16:43:58 +0200

    Production website deployed

--- docs/js/settings-test.js
+++ docs/js/settings-test.js
@@ -1,8 +1,8 @@
 // Settings for testnet (SmartBCH Amber, NetID 10001)
 // deployed on testnet from 0xE0d61627d14F1E6C1C5FABDD219dF0a003C9a0cC
 
-window.RewardTokenAddress  = '0x95c3adFE28E2BAB89B285AB058c450F61B938661';
-window.ReaperAddress       = '0xA995DfA2fcb3623e991B20BE91DAcE4dAd47AB8A';
+window.RewardTokenAddress  = '0x04B51E3DAfa23Bb7a5395fc631b69258191B7B88';
+window.ReaperAddress       = '0xC6b49F643e9E67157D51076736AC7351d30B3057';
 window.DevAddress          = '0xE0d61627d14F1E6C1C5FABDD219dF0a003C9a0cC';
 window.rpcServer           = 'https://moeing.tech:9545';
 window.netId               = 10001;
