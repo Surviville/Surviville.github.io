3c6f4da510cf4e3a232f33adad121609adc9b3a8
Surviville <ultimatesurviville@gmail.com>
2022-07-30 19:01:18 +0200

    Fixed small bug with checking if user has enough SVC before upgrading, added links

--- docs/js/settings-test.js
+++ docs/js/settings-test.js
@@ -1,6 +1,7 @@
 // Settings for testnet (SmartBCH Amber, NetID 10001)
 // deployed on testnet from 0xE0d61627d14F1E6C1C5FABDD219dF0a003C9a0cC
 
+window.SvcStakingAddress    = '';
 window.CardsAddress         = '0x6C2F8FE98E6312CceCF064a846EC3eB83f190124';
 window.RewardTokenAddress   = '0x04B51E3DAfa23Bb7a5395fc631b69258191B7B88';
 window.ReaperAddress        = '0xC6b49F643e9E67157D51076736AC7351d30B3057';
