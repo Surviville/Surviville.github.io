e52237f832e7937cb88b1f462583e7ddb7807406
Surviville <ultimatesurviville@gmail.com>
2022-08-03 19:32:52 +0200

    Added animated headlines

--- docs/js/settings-test.js
+++ docs/js/settings-test.js
@@ -1,11 +1,15 @@
 // Settings for testnet (SmartBCH Amber, NetID 10001)
 // deployed on testnet from 0xE0d61627d14F1E6C1C5FABDD219dF0a003C9a0cC
 
-window.SvcStakingAddress    = '';
-window.CardsAddress         = '0x6C2F8FE98E6312CceCF064a846EC3eB83f190124';
-window.RewardTokenAddress   = '0x04B51E3DAfa23Bb7a5395fc631b69258191B7B88';
-window.ReaperAddress        = '0xC6b49F643e9E67157D51076736AC7351d30B3057';
-window.DevAddress           = '0xE0d61627d14F1E6C1C5FABDD219dF0a003C9a0cC';
-window.rpcServer            = 'https://moeing.tech:9545';
-window.netId                = 10001;
-window.netName              = "SmartBch Testnet";
+window.RT30StakingAddress = '';
+window.RT14StakingAddress = '';
+window.RT7StakingAddress = '';
+window.RT0StakingAddress = '0x60379Be2c107Bd9E23BaE62982db9BeBC73320aF';
+window.CardsAddress = '0x8804066771eE79c4B619B691b5B5c80c4e9580E1';
+
+window.RewardTokenAddress = '0x04B51E3DAfa23Bb7a5395fc631b69258191B7B88'; // SVC
+window.ReaperAddress = '0xC6b49F643e9E67157D51076736AC7351d30B3057';
+window.DevAddress = '0xE0d61627d14F1E6C1C5FABDD219dF0a003C9a0cC';
+window.rpcServer = 'https://moeing.tech:9545';
+window.netId = 10001;
+window.netName = "SmartBch Testnet";
