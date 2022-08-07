27b26cef825f9898524497292b07d258705fbe22
Surviville <ultimatesurviville@gmail.com>
2022-08-07 14:41:39 +0200

    Popup margin fix

--- docs/js/settings-test.js
+++ docs/js/settings-test.js
@@ -5,7 +5,7 @@ window.RT30StakingAddress = '';
 window.RT14StakingAddress = '';
 window.RT7StakingAddress = '';
 window.RT0StakingAddress = '0x60379Be2c107Bd9E23BaE62982db9BeBC73320aF';
-window.CardsAddress = '0x8804066771eE79c4B619B691b5B5c80c4e9580E1';
+window.CardsAddress = '0x2e825888bB89fFcaA68f0b6A7EB19D334C5f885B';
 
 window.RewardTokenAddress = '0x04B51E3DAfa23Bb7a5395fc631b69258191B7B88'; // SVC
 window.ReaperAddress = '0xC6b49F643e9E67157D51076736AC7351d30B3057';
