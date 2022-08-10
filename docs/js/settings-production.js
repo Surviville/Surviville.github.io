a67bac24266be5513bb559d4de5e50e3551afb44
Surviville <ultimatesurviville@gmail.com>
2022-08-10 17:09:36 +0200

    Mainnet version

--- docs/js/settings-production.js
+++ docs/js/settings-production.js
@@ -1,6 +1,7 @@
 // Settings for testnet (SmartBCH Amber, NetID 10001)
 // deployed on testnet from 0xE0d61627d14F1E6C1C5FABDD219dF0a003C9a0cC
 
+window.ca = {};
 window.ca['cards'] = '0xE7B1076d0853a7aAa6FbB477886726ed03C6Fffd'; // wrong address -- not deployed yet
 window.ca['svc'] = '0xE7B1076d0853a7aAa6FbB477886726ed03C6Fffd';
 window.ca['reaper'] = '0xb1C2103F29341C0E62aE88a5d13D84b5ea6757A9';
